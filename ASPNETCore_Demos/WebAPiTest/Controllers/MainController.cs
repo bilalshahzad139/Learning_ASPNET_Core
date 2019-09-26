88372225d87d21a67fa49a5c7df05607d64d8329
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-09-26 14:39:47 +0500

    web api, jwt, claim data

---
+++ ASPNETCore_Demos/WebAPiTest/Controllers/MainController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPiTest.Controllers
+{
+
+    [Route("api/main")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("gettoken")]
+        public Object GetToken()
+        {
+            string key = "my_secret_key_12345";
+            var issuer = "http://mysite.com";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var permClaims = new List<Claim>();
+            permClaims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            permClaims.Add(new Claim("valid", "1"));
+            permClaims.Add(new Claim("userid", "1"));
+            permClaims.Add(new Claim("name", "bilal"));
+
+            var token = new JwtSecurityToken(issuer,
+                            issuer,
+                            permClaims,
+                            expires: DateTime.Now.AddDays(1),
+                            signingCredentials: credentials);
+            var jwt_token = new JwtSecurityTokenHandler().WriteToken(token);
+            return new { data = jwt_token };
+
+
+        }
+
+
+        [HttpPost("getname1")]
+        public ActionResult<String> GetName1()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var claims = User.Claims;
+                return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+        }
+
+        [Authorize]
+        [HttpPost("getname2")]
+        public ActionResult<String> GetName2()
+        {
+            var claims = User.Claims;
+            return "";
+        }
+
+
+        [Authorize(Policy = "IsValid")]
+        [HttpPost("getname3")]
+        public ActionResult<String> GetName3()
+        {
+            var claims = User.Claims;
+            foreach (var claim in claims)
+            {
+                if (claim.Type == "name")
+                {
+                    return claim.Value;
+                }
+            }
+            return "";
+        }
+
+        [ClaimDTOAttribute]
+        [Authorize(Policy = "IsValid")]
+        [HttpPost("getname4")]
+        public Object GetName4([FromHeader] ClaimDTO claimDto)
+        {
+            String name = "";
+            if (claimDto != null)
+                name = claimDto.FullName;
+
+            return new
+            {
+                data = name
+            };
+        }
+    }
+
+    public class ClaimDTO
+    {
+        public int UserID { get; set; }
+        public String FullName { get; set; }
+    }
+    public class ClaimDTOAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var dto = ((ClaimDTO)context.ActionArguments["claimDto"]);
+            var claimsIdentity = context.HttpContext.User.Identity as ClaimsIdentity;
+            dto.UserID = Convert.ToInt32(claimsIdentity.Claims.FirstOrDefault(c => c.Type == "userid")?.Value);
+            dto.FullName = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "name").Value;
+        }
+    }
+}
---
+++ ASPNETCore_Demos/WebAPiTest/Startup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPiTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("default",
+                        builder =>
+                        {
+                            //builder.WithOrigins("*").AllowAnyHeader()
+                            //            .AllowAnyMethod();
+
+                            builder.AllowAnyOrigin().AllowAnyHeader()
+                                            .AllowAnyMethod();
+                        });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = "http://mysite.com", //some string, normally web url,
+                   ValidAudience = "http://mysite.com",
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_secret_key_12345"))
+               };
+
+               options.Events = new JwtBearerEvents
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                       {
+                           context.Response.Headers.Add("Token-Expired", "true");
+                       }
+                       return Task.CompletedTask;
+                   }
+               };
+           });
+
+            services.AddAuthorization(opts =>
+            {
+                opts.AddPolicy("IsValid", p =>
+                {
+                    p.RequireClaim("valid", "1");
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseCors("default");
+            app.UseMvc();
+        }
+    }
+}
